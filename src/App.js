f567d21215340ee0e77b048bc45f94d349f650df
valensixx <valensixx@gmail.com>
2023-09-14 13:23:17 +0300

    add and connect components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
 
 import(Routes)
 function App() {
   return (
    <Routes>
       <Route path="/" element={<Layout/>}>
-        
+        <Route index element={<Public/>}>
+          <Route path="login" element={<Login/>}>
+            
+          </Route>
+        </Route>
       </Route>
    </Routes>
   );
---
+++ src/components/Public.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+const Public = () => {
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Welcome to <span className="nowrap">TechNotes Repairs!</span></h1>
+            </header>
+            <main className="public__main">
+                <p>Located in Beautiful Town Plovdiv, Valensixx  provides a trained staff ready to meet your tech repair needs.</p>
+                <address className="public__addr">
+                    Valensixx<br />
+                    20 Foo Drama<br />
+                    Plovdiv City, BG 12345<br />
+                    <a href="tel:+15555555555">(555) 555-5555</a>
+                </address>
+                <br />
+                <p>Owner: Dan Davidson</p>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+
+    )
+    return content
+}
+export default Public;
