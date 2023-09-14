bdd8a3bc094d43eeb55b79c37fda5692e8771812
valensixx <valensixx@gmail.com>
2023-09-14 14:04:46 +0300

    components done for now

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./components/Login";
+import DashLayout from "./components/DashLayout";
 
 import(Routes)
 function App() {
   return (
    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Public/>}>
-          <Route path="login" element={<Login/>}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />}>
+          <Route path="login" element={<Login />}>
+            
+            <Route path="dash" element={<DashLayout/>}>
+              
+            </Route>
             
           </Route>
         </Route>
