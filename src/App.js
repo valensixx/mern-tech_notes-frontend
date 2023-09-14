ff59f2ddfa110328892d6c66982031cd6b940cdf
valensixx <valensixx@gmail.com>
2023-09-14 13:03:12 +0300

    working on file structure for the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
+import(Routes)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+      <Route path="/" element={<Layout/>}>
+        
+      </Route>
+   </Routes>
   );
 }
 
