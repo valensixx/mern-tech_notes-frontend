ce6b3aaa349e9d06a9b31cbd7d8f4d965ae5af4c
valensixx <valensixx@gmail.com>
2023-09-15 11:01:15 +0300

    UsersList done, working on User.js

---
+++ src/features/users/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+import { selectUserById } from "./usersApiSlice";
+
+const User = () => {
+  return (
+    <div>User</div>
+  )
+}
+
+export default User;
