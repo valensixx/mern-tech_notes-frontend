26bf102f5bd4a191d5de9e656fe0dde4ede6b505
valensixx <valensixx@gmail.com>
2023-09-15 11:28:47 +0300

    fixed error

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
-import { createApi, fetchBaseQuerty} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const apiSlice = createApi ({
-    baseQuery: fetchBaseQuerty({ beseUrl: 'http://localhost:3500' }),
+export const apiSlice = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
-});
+})
