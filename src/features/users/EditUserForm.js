62c6e3c5bea042be1d9e27b34cf5a2befd9af5f2
valensixx <valensixx@gmail.com>
2023-09-18 15:44:25 +0300

    working in EditUserForm

--- src/features/users/EditUserForm.js
+++ src/features/users/EditUserForm.js
@@ -4,3 +4,57 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
+
+const USER_REGEX = /^[A-z]{3,20}$/
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
+
+const EditUserForm = ({user}) => {
+
+    const [updateUser, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateUserMutation()
+
+    const [deleteUser, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delerror
+    }] = useDeleteUserMutation()
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState(user.username)
+    const [validUsername, setValidUsername] = useState(false)
+    const [password, setPassword] = useState('')
+    const [validPassword, setValidPassword] = useState(false)
+    const [roles, setRoles] = useState(user.roles)
+    const [active, setActive] = useState(user.active)
+
+    useEffect(()=>{
+        setValidUsername(USER_REGEX.test(username))
+    }, [username])
+
+    useEffect(()=>{
+        setValidPassword(USER_REGEX.test(password))
+    }, [password])
+
+    useEffect(() => {
+        if(isSuccess || isDelSuccess){
+            setUsername('')
+            setPassword('')
+            setRoles([])
+            navigate('/dash/users')
+        }
+
+    }, [isSuccess, isDelSuccess, navigate])
+
+    
+
+  return (
+    <div>EditUserForm</div>
+  )
+}
+
+export default EditUserForm
