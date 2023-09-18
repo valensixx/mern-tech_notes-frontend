213c1a0aaefbe00707d8ff2edccffb3eda042194
valensixx <valensixx@gmail.com>
2023-09-18 13:01:48 +0300

    changes in App.js and add roles in roles.js

--- src/App.js
+++ src/App.js
@@ -24,10 +24,14 @@ function App() {
 
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
           </Route>
 
           <Route path="notes">
             <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
 
         </Route>{/* End Dash */}
