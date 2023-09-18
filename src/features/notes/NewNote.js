55065dacda79784f3f0068e1164b4e3a56c0e958
valensixx <valensixx@gmail.com>
2023-09-18 13:18:10 +0300

    working on new user form

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -1,12 +1,7 @@
 import React from "react"
-import { useState, useEffect } from "react"
-import { useAddNewUserMutation } from "./notesApiSlice"
-import { useNavigate } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSave } from "@fortawesome/free-solid-svg-icons"
-import { ROLES } from "../../config/roles"
 
 const NewNote = () => {
+  
   return (
     <div>NewNote</div>
   )
--- src/features/users/NewUserForm.js
+++ src/features/users/NewUserForm.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React from "react"
+import { useState, useEffect } from "react"
+import { useAddNewUserMutation } from "./notesApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSave } from "@fortawesome/free-solid-svg-icons"
+import { ROLES } from "../../config/roles"
+
+const USER_REGEX = /^[A-z]{3,20}$/
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
 
 const NewUserForm = () => {
+
+  const [addNewUser,{
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }] = useAddNewUserMutation()
+
+  const navigate = useNavigate()
+
+  const [username, SetUsername] = useState('')
+  const [validUsername, setValidUsername] = useState(false)
+  const [password, setPassword] = useState('')
+  const [validPassword, setValidPassword] = useState(false)
+  const [roles, setRoles] = useState(["Employee"])
+
   return (
     <div>NewUserForm</div>
   )
