e755f0243c1d78c23ab66192943adf0c826ebc89
valensixx <valensixx@gmail.com>
2023-09-18 15:19:54 +0300

    working on EditUser and EditUserForm

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react"
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectUserById } from "./usersApiSlice"
 
 const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
+  const {id} = useParams()
+
+  const user = useSelector(state => selectUserById(state,id))
+
+  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+
+  return content
 }
 
 export default EditUser
---
+++ src/features/users/EditUserForm.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import { useUpdateUserMutation, useDeleteUserMutation } from "../notes/notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { ROLES } from "../../config/roles";
