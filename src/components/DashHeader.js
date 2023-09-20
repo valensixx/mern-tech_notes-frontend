c7714ef929aef2a777aaa848e967832ec64be956
valensixx <valensixx@gmail.com>
2023-09-21 02:37:58 +0300

    component done

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -30,16 +30,32 @@ const DashHeader = () => {
 
     if (isLoading) return <p>Loading Out...</p>
 
-    if(isError) return <p>Error: {error.message}</p>
+    if(isError) return <p>Error: {error.data?.message}</p>
+
+    let dashClass = null 
+    if(!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+        dashClass = "dash-header__container--small"
+    }
+
+    const logoutButton = (
+        <button
+            className="icon-button"
+            title="Logout"
+            onClick={onLogoutClicked}
+        >
+            <FontAwesomeIcon icon={faRightFromBracket} />
+        </button>
+    )
 
     const content = (
         <header className="dash-header">
-            <div className="dash-header__container">
+            <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
                     <h1 className="dash-header__title">techNotes</h1>
                 </Link>
                 <nav className="dash-header__nav">
-                    {/* will add nav buttons later */}
+                    {/* will add more buttons later */}
+                    {logoutButton}
                 </nav>
             </div>
         </header>
