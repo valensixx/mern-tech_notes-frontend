f5f20059a35075e32abfe713e2516159c22fd499
valensixx <valensixx@gmail.com>
2023-09-21 02:29:39 +0300

    working on DashHeader

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -4,7 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 
+import { useSendLogoutMutation } from "../features/auth/authApiSlice"
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
+
 const DashHeader = () => {
+
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation()
+
+    useEffect(() => {
+        if (isSuccess) navigate('/')
+    }, [isSuccess, navigate])
+
+    const onLogoutClicked = () => sendLogout()
+
+    if (isLoading) return <p>Loading Out...</p>
+
+    if(isError) return <p>Error: {error.message}</p>
+
     const content = (
         <header className="dash-header">
             <div className="dash-header__container">
