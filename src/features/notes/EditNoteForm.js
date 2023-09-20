90cb2b918e8f0d62c8d291a88282d1895a424d5e
valensixx <valensixx@gmail.com>
2023-09-20 13:15:05 +0300

    upload changes

---
+++ src/features/notes/EditNoteForm.js
@@ -0,0 +1,73 @@
+import React from "react"
+import { useUpdateUserMutation, useDeleteUserMutation } from "./notesApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
+
+const EditNoteForm = ({note, users}) => {
+
+    const [updateNote, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateUserMutation()
+
+    const [deleteNote, {
+        isSuccess: isDelSuccesss,
+        isError: isDelError,
+        error: delerror
+    }] = useDeleteUserMutation()
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState(note.title)
+    const [text, setText] = useState(note.text)
+    const [completed, setCompleted] = useState(note.completed)
+    const [userId, setUserId] = useState(note.user)
+
+    useEffect(() => {
+
+        if (isSuccess || isDelSuccess) {
+            setTitle('')
+            setText('')
+            setUserId('')
+            navigate('/dash/notes')
+        }
+
+    }, [isSuccess, isDelSuccess, navigate])
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onTextChanged = e => setText(e.target.value)
+    const onCompletedChanged = e => setCompleted(prev => !prev)
+    const onUserIdChanged = e => setUserId(e.target.value)
+
+    const canSave = [title, text, userId].every(Boolean) && !isLoading
+
+    const onSaveNoteClicked = async (e) => {
+        if (canSave) {
+            await updateNote({ id: note.id, user: userId, title, text, completed })
+        }
+    }
+
+    const onDeleteNoteClicked = async () => {
+        await deleteNote({ id: note.id })
+    }
+
+    const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+    const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+
+    const options = users.map(user => {
+        return (
+            <option
+                key={user.id}
+                value={user.id}
+
+            > {user.username}</option >
+        )
+    })
+
+    
+}
+
+export default EditNoteForm
