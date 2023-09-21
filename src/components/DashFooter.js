d2e8d4d0a4002fef3828915b4419fef97780979f
valensixx <valensixx@gmail.com>
2023-09-21 13:50:13 +0300

    changes

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate, useLocation } from "react-router-dom";
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate, useLocation } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 const DashFooter = () => {
 
-    const navigate = useNavigate();
-    const {pathname} = useLocation();
+    const {username, status} = useAuth()    
 
-    const onGoHomeClicked = () => navigate('/dash');
+    const navigate = useNavigate()
+    const {pathname} = useLocation()
+
+    const onGoHomeClicked = () => navigate('/dash')
 
     let goHomeButton = null;
     if(pathname !== '/dash'){
@@ -26,11 +29,11 @@ const DashFooter = () => {
     const content = (
         <footer className="dash-footer">
             {goHomeButton}
-            <p>Current User:</p>
-            <p>Status:</p>
+            <p>Current User: {username}</p>
+            <p>Status: {status}</p>
         </footer>
     )
-  return content;
+  return content
 }
 
-export default DashFooter;
+export default DashFooter
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -8,6 +8,20 @@ const useAuth = () => {
     let isAdmin = false 
     let status = "Employee"
 
+    if(token) {
+        const decoded = jwtDecode(token)
+        const { username, roles } = decoded.UserInfo
+
+        isManager = roles.includes('Manager')
+        isAdmin = roles.includes('Admin')
+
+        if (isManager) status = "Manager"
+        if (isAdmin) status = "Admin"
+
+        return { username, roles, status, isManager, isAdmin }
+    }
+
+    return { username: '', roles: [], isManager, isAdmin, status }
 }
 
 export default useAuth
