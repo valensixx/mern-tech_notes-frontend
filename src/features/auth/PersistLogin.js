5f9d83bb1c28613a32c5895ab08c6eb02019df11
valensixx <valensixx@gmail.com>
2023-09-21 13:32:24 +0300

    changes

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -46,9 +46,31 @@ const PersistLogin = () => {
         // eslint-disable-next-line
     }, [])
 
-  return (
-    <div>PersistLogin</div>
-  )
+    let content
+    if (!persist) { // persist: no
+        console.log('no persist')
+        content = <Outlet />
+    } else if (isLoading) { //persist: yes, token: no
+        console.log('loading')
+        content = <p>Loading...</p>
+    } else if (isError) { //persist: yes, token: no
+        console.log('error')
+        content = (
+            <p className='errmsg'>
+                {error.data?.message}
+                <Link to="/login">Please login again</Link>.
+            </p>
+        )
+    } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
+        console.log('success')
+        content = <Outlet />
+    } else if (token && isUninitialized) { //persist: yes, token: yes
+        console.log('token and uninit')
+        console.log(isUninitialized)
+        content = <Outlet />
+    }
+
+  return content
 }
 
 export default PersistLogin
