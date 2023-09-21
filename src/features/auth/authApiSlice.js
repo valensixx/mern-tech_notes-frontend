6e77a296d407eb9e4d7ec816dc4d3f0f3ab6f146
valensixx <valensixx@gmail.com>
2023-09-21 12:24:03 +0300

    usePersist.js

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "../../app/api/apiSlice"
-import { logOut, setCredentials } from "./authSlice"
+import { logOut } from "./authSlice"
 
 export const authApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
@@ -17,12 +17,11 @@ export const authApiSlice = apiSlice.injectEndpoints({
             }),
             async onQueryStarted(arg, { dispatch, queryFulfilled }) {
                 try {
-                    const { data } = await queryFulfilled
-                    console.log(data)
+                    //const { data } = 
+                    await queryFulfilled
+                    //console.log(data)
                     dispatch(logOut())
-                    setTimeout(() => {
-                        dispatch(apiSlice.util.resetApiState())
-                    }, 1000)
+                    dispatch(apiSlice.util.resetApiState())
                 } catch (err) {
                     console.log(err)
                 }
@@ -32,17 +31,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: '/auth/refresh',
                 method: 'GET',
-            }),
-            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-                try {
-                    const { data } = await queryFulfilled
-                    console.log(data)
-                    const { accessToken } = data
-                    dispatch(setCredentials({ accessToken }))
-                } catch (err) {
-                    console.log(err)
-                }
-            }
+            })
         }),
     })
 })
