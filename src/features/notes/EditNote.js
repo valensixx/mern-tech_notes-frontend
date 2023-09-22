441cdf89e91b5a980bc455257ac831327a28ef18
valensixx <valensixx@gmail.com>
2023-09-23 00:48:35 +0300

    final changes in frontend project

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -1,20 +1,41 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux/es/hooks/useSelector"
-import { selectNoteById } from "./notesApiSlice"
-import { selectAllUsers } from "../users/usersApiSlice"
-import EditNoteForm from "./EditNoteForm"
-
+import { useParams } from 'react-router-dom'
+import EditNoteForm from './EditNoteForm'
+import { useGetNotesQuery } from './notesApiSlice'
+import { useGetUsersQuery } from '../users/usersApiSlice'
+import useAuth from '../../hooks/useAuth'
+import PulseLoader from 'react-spinners/PulseLoader'
+import useTitle from '../../hooks/useTitle'
 
 const EditNote = () => {
-  const {id} = useParams()
+    useTitle('techNotes: Edit Note')
 
-  const note = useSelector(state => selectNoteById(state, id))
-  const users = useSelector(selectAllUsers)
+    const { id } = useParams()
 
-  const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading....</p>
+    const { username, isManager, isAdmin } = useAuth()
 
-  return content
+    const { note } = useGetNotesQuery("notesList", {
+        selectFromResult: ({ data }) => ({
+            note: data?.entities[id]
+        }),
+    })
+
+    const { users } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            users: data?.ids.map(id => data?.entities[id])
+        }),
+    })
+
+    if (!note || !users?.length) return <PulseLoader color={"#FFF"} />
+
+
+    if (!isManager && !isAdmin) {
+        if (note.username !== username) {
+            return <p className="errmsg">No access</p>
+        }
+    }
+
+    const content = <EditNoteForm note={note} users={users} />
+
+    return content
 }
-
 export default EditNote
--- src/features/users/User.js
+++ src/features/users/User.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate } from 'react-router-dom'
-
 import { useSelector } from 'react-redux'
 import { selectUserById } from './usersApiSlice'
+import { memo } from 'react'
 
 const User = ({ userId }) => {
     const user = useSelector(state => selectUserById(state, userId))
@@ -34,4 +34,7 @@ const User = ({ userId }) => {
 
     } else return null
 }
-export default User
+
+const memorizedUser = memo(User)
+
+export default memorizedUser
